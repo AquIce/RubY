e425686820d4eb991be88847b30fb2d52ea61925
AquIce <timeo.perrinjaquet@gmail.com>
2023-09-09 22:40:04 +0200

    Added title and fixed tab order

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -114,7 +114,7 @@
             this.btnU2.Location = new System.Drawing.Point(533, 151);
             this.btnU2.Name = "btnU2";
             this.btnU2.Size = new System.Drawing.Size(60, 28);
-            this.btnU2.TabIndex = 0;
+            this.btnU2.TabIndex = 2;
             this.btnU2.Text = "U2";
             this.btnU2.UseVisualStyleBackColor = true;
             this.btnU2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -125,7 +125,7 @@
             this.btnU.Location = new System.Drawing.Point(533, 89);
             this.btnU.Name = "btnU";
             this.btnU.Size = new System.Drawing.Size(60, 29);
-            this.btnU.TabIndex = 1;
+            this.btnU.TabIndex = 0;
             this.btnU.Text = "U";
             this.btnU.UseVisualStyleBackColor = true;
             this.btnU.Click += new System.EventHandler(this.btnRotate_Click);
@@ -136,7 +136,7 @@
             this.btnUprime.Location = new System.Drawing.Point(533, 120);
             this.btnUprime.Name = "btnUprime";
             this.btnUprime.Size = new System.Drawing.Size(60, 29);
-            this.btnUprime.TabIndex = 2;
+            this.btnUprime.TabIndex = 1;
             this.btnUprime.Text = "U\'";
             this.btnUprime.UseVisualStyleBackColor = true;
             this.btnUprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -158,7 +158,7 @@
             this.btnDw.Location = new System.Drawing.Point(615, 181);
             this.btnDw.Name = "btnDw";
             this.btnDw.Size = new System.Drawing.Size(60, 27);
-            this.btnDw.TabIndex = 10;
+            this.btnDw.TabIndex = 7;
             this.btnDw.Text = "d";
             this.btnDw.UseVisualStyleBackColor = true;
             this.btnDw.Click += new System.EventHandler(this.btnRotate_Click);
@@ -169,7 +169,7 @@
             this.btnDprime.Location = new System.Drawing.Point(615, 120);
             this.btnDprime.Name = "btnDprime";
             this.btnDprime.Size = new System.Drawing.Size(60, 29);
-            this.btnDprime.TabIndex = 9;
+            this.btnDprime.TabIndex = 5;
             this.btnDprime.Text = "D\'";
             this.btnDprime.UseVisualStyleBackColor = true;
             this.btnDprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -180,7 +180,7 @@
             this.btnD.Location = new System.Drawing.Point(615, 89);
             this.btnD.Name = "btnD";
             this.btnD.Size = new System.Drawing.Size(60, 29);
-            this.btnD.TabIndex = 8;
+            this.btnD.TabIndex = 4;
             this.btnD.Text = "D";
             this.btnD.UseVisualStyleBackColor = true;
             this.btnD.Click += new System.EventHandler(this.btnRotate_Click);
@@ -191,7 +191,7 @@
             this.btnD2.Location = new System.Drawing.Point(615, 151);
             this.btnD2.Name = "btnD2";
             this.btnD2.Size = new System.Drawing.Size(60, 28);
-            this.btnD2.TabIndex = 7;
+            this.btnD2.TabIndex = 6;
             this.btnD2.Text = "D2";
             this.btnD2.UseVisualStyleBackColor = true;
             this.btnD2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -202,7 +202,7 @@
             this.btnRw.Location = new System.Drawing.Point(697, 181);
             this.btnRw.Name = "btnRw";
             this.btnRw.Size = new System.Drawing.Size(60, 27);
-            this.btnRw.TabIndex = 14;
+            this.btnRw.TabIndex = 11;
             this.btnRw.Text = "r";
             this.btnRw.UseVisualStyleBackColor = true;
             this.btnRw.Click += new System.EventHandler(this.btnRotate_Click);
@@ -213,7 +213,7 @@
             this.btnRprime.Location = new System.Drawing.Point(697, 120);
             this.btnRprime.Name = "btnRprime";
             this.btnRprime.Size = new System.Drawing.Size(60, 29);
-            this.btnRprime.TabIndex = 13;
+            this.btnRprime.TabIndex = 9;
             this.btnRprime.Text = "R\'";
             this.btnRprime.UseVisualStyleBackColor = true;
             this.btnRprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -224,7 +224,7 @@
             this.btnR.Location = new System.Drawing.Point(697, 89);
             this.btnR.Name = "btnR";
             this.btnR.Size = new System.Drawing.Size(60, 29);
-            this.btnR.TabIndex = 12;
+            this.btnR.TabIndex = 8;
             this.btnR.Text = "R";
             this.btnR.UseVisualStyleBackColor = true;
             this.btnR.Click += new System.EventHandler(this.btnRotate_Click);
@@ -235,7 +235,7 @@
             this.btnR2.Location = new System.Drawing.Point(697, 151);
             this.btnR2.Name = "btnR2";
             this.btnR2.Size = new System.Drawing.Size(60, 28);
-            this.btnR2.TabIndex = 11;
+            this.btnR2.TabIndex = 10;
             this.btnR2.Text = "R2";
             this.btnR2.UseVisualStyleBackColor = true;
             this.btnR2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -246,7 +246,7 @@
             this.btnLw.Location = new System.Drawing.Point(777, 181);
             this.btnLw.Name = "btnLw";
             this.btnLw.Size = new System.Drawing.Size(60, 27);
-            this.btnLw.TabIndex = 18;
+            this.btnLw.TabIndex = 15;
             this.btnLw.Text = "l";
             this.btnLw.UseVisualStyleBackColor = true;
             this.btnLw.Click += new System.EventHandler(this.btnRotate_Click);
@@ -257,7 +257,7 @@
             this.btnLprime.Location = new System.Drawing.Point(777, 120);
             this.btnLprime.Name = "btnLprime";
             this.btnLprime.Size = new System.Drawing.Size(60, 29);
-            this.btnLprime.TabIndex = 17;
+            this.btnLprime.TabIndex = 13;
             this.btnLprime.Text = "L\'";
             this.btnLprime.UseVisualStyleBackColor = true;
             this.btnLprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -268,7 +268,7 @@
             this.btnL.Location = new System.Drawing.Point(777, 89);
             this.btnL.Name = "btnL";
             this.btnL.Size = new System.Drawing.Size(60, 29);
-            this.btnL.TabIndex = 16;
+            this.btnL.TabIndex = 12;
             this.btnL.Text = "L";
             this.btnL.UseVisualStyleBackColor = true;
             this.btnL.Click += new System.EventHandler(this.btnRotate_Click);
@@ -279,7 +279,7 @@
             this.btnL2.Location = new System.Drawing.Point(777, 151);
             this.btnL2.Name = "btnL2";
             this.btnL2.Size = new System.Drawing.Size(60, 28);
-            this.btnL2.TabIndex = 15;
+            this.btnL2.TabIndex = 14;
             this.btnL2.Text = "L2";
             this.btnL2.UseVisualStyleBackColor = true;
             this.btnL2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -290,7 +290,7 @@
             this.btnFw.Location = new System.Drawing.Point(853, 181);
             this.btnFw.Name = "btnFw";
             this.btnFw.Size = new System.Drawing.Size(60, 27);
-            this.btnFw.TabIndex = 22;
+            this.btnFw.TabIndex = 19;
             this.btnFw.Text = "f";
             this.btnFw.UseVisualStyleBackColor = true;
             this.btnFw.Click += new System.EventHandler(this.btnRotate_Click);
@@ -301,7 +301,7 @@
             this.btnFprime.Location = new System.Drawing.Point(853, 120);
             this.btnFprime.Name = "btnFprime";
             this.btnFprime.Size = new System.Drawing.Size(60, 29);
-            this.btnFprime.TabIndex = 21;
+            this.btnFprime.TabIndex = 17;
             this.btnFprime.Text = "F\'";
             this.btnFprime.UseVisualStyleBackColor = true;
             this.btnFprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -312,7 +312,7 @@
             this.btnF.Location = new System.Drawing.Point(853, 89);
             this.btnF.Name = "btnF";
             this.btnF.Size = new System.Drawing.Size(60, 29);
-            this.btnF.TabIndex = 20;
+            this.btnF.TabIndex = 16;
             this.btnF.Text = "F";
             this.btnF.UseVisualStyleBackColor = true;
             this.btnF.Click += new System.EventHandler(this.btnRotate_Click);
@@ -323,7 +323,7 @@
             this.btnF2.Location = new System.Drawing.Point(853, 151);
             this.btnF2.Name = "btnF2";
             this.btnF2.Size = new System.Drawing.Size(60, 28);
-            this.btnF2.TabIndex = 19;
+            this.btnF2.TabIndex = 18;
             this.btnF2.Text = "F2";
             this.btnF2.UseVisualStyleBackColor = true;
             this.btnF2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -334,7 +334,7 @@
             this.btnBw.Location = new System.Drawing.Point(927, 181);
             this.btnBw.Name = "btnBw";
             this.btnBw.Size = new System.Drawing.Size(60, 27);
-            this.btnBw.TabIndex = 26;
+            this.btnBw.TabIndex = 23;
             this.btnBw.Text = "b";
             this.btnBw.UseVisualStyleBackColor = true;
             this.btnBw.Click += new System.EventHandler(this.btnRotate_Click);
@@ -345,7 +345,7 @@
             this.btnBprime.Location = new System.Drawing.Point(927, 120);
             this.btnBprime.Name = "btnBprime";
             this.btnBprime.Size = new System.Drawing.Size(60, 29);
-            this.btnBprime.TabIndex = 25;
+            this.btnBprime.TabIndex = 21;
             this.btnBprime.Text = "B\'";
             this.btnBprime.UseVisualStyleBackColor = true;
             this.btnBprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -356,7 +356,7 @@
             this.btnB.Location = new System.Drawing.Point(927, 89);
             this.btnB.Name = "btnB";
             this.btnB.Size = new System.Drawing.Size(60, 29);
-            this.btnB.TabIndex = 24;
+            this.btnB.TabIndex = 20;
             this.btnB.Text = "B";
             this.btnB.UseVisualStyleBackColor = true;
             this.btnB.Click += new System.EventHandler(this.btnRotate_Click);
@@ -367,7 +367,7 @@
             this.btnB2.Location = new System.Drawing.Point(927, 151);
             this.btnB2.Name = "btnB2";
             this.btnB2.Size = new System.Drawing.Size(60, 28);
-            this.btnB2.TabIndex = 23;
+            this.btnB2.TabIndex = 22;
             this.btnB2.Text = "B2";
             this.btnB2.UseVisualStyleBackColor = true;
             this.btnB2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -378,7 +378,7 @@
             this.btnxprime.Location = new System.Drawing.Point(777, 331);
             this.btnxprime.Name = "btnxprime";
             this.btnxprime.Size = new System.Drawing.Size(60, 27);
-            this.btnxprime.TabIndex = 29;
+            this.btnxprime.TabIndex = 34;
             this.btnxprime.Text = "x\'";
             this.btnxprime.UseVisualStyleBackColor = true;
             this.btnxprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -389,7 +389,7 @@
             this.btnx.Location = new System.Drawing.Point(777, 301);
             this.btnx.Name = "btnx";
             this.btnx.Size = new System.Drawing.Size(60, 27);
-            this.btnx.TabIndex = 28;
+            this.btnx.TabIndex = 33;
             this.btnx.Text = "x";
             this.btnx.UseVisualStyleBackColor = true;
             this.btnx.Click += new System.EventHandler(this.btnRotate_Click);
@@ -400,7 +400,7 @@
             this.btnx2.Location = new System.Drawing.Point(777, 362);
             this.btnx2.Name = "btnx2";
             this.btnx2.Size = new System.Drawing.Size(60, 27);
-            this.btnx2.TabIndex = 27;
+            this.btnx2.TabIndex = 35;
             this.btnx2.Text = "x2";
             this.btnx2.UseVisualStyleBackColor = true;
             this.btnx2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -411,7 +411,7 @@
             this.btnyprime.Location = new System.Drawing.Point(853, 331);
             this.btnyprime.Name = "btnyprime";
             this.btnyprime.Size = new System.Drawing.Size(60, 27);
-            this.btnyprime.TabIndex = 32;
+            this.btnyprime.TabIndex = 37;
             this.btnyprime.Text = "y\'";
             this.btnyprime.UseVisualStyleBackColor = true;
             this.btnyprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -422,7 +422,7 @@
             this.btny.Location = new System.Drawing.Point(853, 301);
             this.btny.Name = "btny";
             this.btny.Size = new System.Drawing.Size(60, 27);
-            this.btny.TabIndex = 31;
+            this.btny.TabIndex = 36;
             this.btny.Text = "y";
             this.btny.UseVisualStyleBackColor = true;
             this.btny.Click += new System.EventHandler(this.btnRotate_Click);
@@ -433,7 +433,7 @@
             this.btny2.Location = new System.Drawing.Point(853, 362);
             this.btny2.Name = "btny2";
             this.btny2.Size = new System.Drawing.Size(60, 27);
-            this.btny2.TabIndex = 30;
+            this.btny2.TabIndex = 38;
             this.btny2.Text = "y2";
             this.btny2.UseVisualStyleBackColor = true;
             this.btny2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -444,7 +444,7 @@
             this.btnzprime.Location = new System.Drawing.Point(927, 331);
             this.btnzprime.Name = "btnzprime";
             this.btnzprime.Size = new System.Drawing.Size(60, 27);
-            this.btnzprime.TabIndex = 35;
+            this.btnzprime.TabIndex = 40;
             this.btnzprime.Text = "z\'";
             this.btnzprime.UseVisualStyleBackColor = true;
             this.btnzprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -455,7 +455,7 @@
             this.btnz.Location = new System.Drawing.Point(927, 301);
             this.btnz.Name = "btnz";
             this.btnz.Size = new System.Drawing.Size(60, 27);
-            this.btnz.TabIndex = 34;
+            this.btnz.TabIndex = 39;
             this.btnz.Text = "z";
             this.btnz.UseVisualStyleBackColor = true;
             this.btnz.Click += new System.EventHandler(this.btnRotate_Click);
@@ -466,7 +466,7 @@
             this.btnz2.Location = new System.Drawing.Point(927, 362);
             this.btnz2.Name = "btnz2";
             this.btnz2.Size = new System.Drawing.Size(60, 27);
-            this.btnz2.TabIndex = 33;
+            this.btnz2.TabIndex = 41;
             this.btnz2.Text = "z2";
             this.btnz2.UseVisualStyleBackColor = true;
             this.btnz2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -477,7 +477,7 @@
             this.btnMprime.Location = new System.Drawing.Point(533, 333);
             this.btnMprime.Name = "btnMprime";
             this.btnMprime.Size = new System.Drawing.Size(60, 29);
-            this.btnMprime.TabIndex = 38;
+            this.btnMprime.TabIndex = 25;
             this.btnMprime.Text = "M\'";
             this.btnMprime.UseVisualStyleBackColor = true;
             this.btnMprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -488,7 +488,7 @@
             this.btnM.Location = new System.Drawing.Point(533, 301);
             this.btnM.Name = "btnM";
             this.btnM.Size = new System.Drawing.Size(60, 29);
-            this.btnM.TabIndex = 37;
+            this.btnM.TabIndex = 24;
             this.btnM.Text = "M";
             this.btnM.UseVisualStyleBackColor = true;
             this.btnM.Click += new System.EventHandler(this.btnRotate_Click);
@@ -499,7 +499,7 @@
             this.btnM2.Location = new System.Drawing.Point(534, 364);
             this.btnM2.Name = "btnM2";
             this.btnM2.Size = new System.Drawing.Size(60, 29);
-            this.btnM2.TabIndex = 36;
+            this.btnM2.TabIndex = 26;
             this.btnM2.Text = "M2";
             this.btnM2.UseVisualStyleBackColor = true;
             this.btnM2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -510,7 +510,7 @@
             this.btnEprime.Location = new System.Drawing.Point(615, 332);
             this.btnEprime.Name = "btnEprime";
             this.btnEprime.Size = new System.Drawing.Size(60, 27);
-            this.btnEprime.TabIndex = 41;
+            this.btnEprime.TabIndex = 28;
             this.btnEprime.Text = "E\'";
             this.btnEprime.UseVisualStyleBackColor = true;
             this.btnEprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -521,7 +521,7 @@
             this.btnE.Location = new System.Drawing.Point(615, 301);
             this.btnE.Name = "btnE";
             this.btnE.Size = new System.Drawing.Size(60, 27);
-            this.btnE.TabIndex = 40;
+            this.btnE.TabIndex = 27;
             this.btnE.Text = "E";
             this.btnE.UseVisualStyleBackColor = true;
             this.btnE.Click += new System.EventHandler(this.btnRotate_Click);
@@ -532,7 +532,7 @@
             this.btnE2.Location = new System.Drawing.Point(615, 362);
             this.btnE2.Name = "btnE2";
             this.btnE2.Size = new System.Drawing.Size(60, 27);
-            this.btnE2.TabIndex = 39;
+            this.btnE2.TabIndex = 29;
             this.btnE2.Text = "E2";
             this.btnE2.UseVisualStyleBackColor = true;
             this.btnE2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -543,7 +543,7 @@
             this.btnSprime.Location = new System.Drawing.Point(697, 332);
             this.btnSprime.Name = "btnSprime";
             this.btnSprime.Size = new System.Drawing.Size(60, 27);
-            this.btnSprime.TabIndex = 44;
+            this.btnSprime.TabIndex = 31;
             this.btnSprime.Text = "S\'";
             this.btnSprime.UseVisualStyleBackColor = true;
             this.btnSprime.Click += new System.EventHandler(this.btnRotate_Click);
@@ -554,7 +554,7 @@
             this.btnS.Location = new System.Drawing.Point(697, 301);
             this.btnS.Name = "btnS";
             this.btnS.Size = new System.Drawing.Size(60, 27);
-            this.btnS.TabIndex = 43;
+            this.btnS.TabIndex = 30;
             this.btnS.Text = "S";
             this.btnS.UseVisualStyleBackColor = true;
             this.btnS.Click += new System.EventHandler(this.btnRotate_Click);
@@ -565,7 +565,7 @@
             this.btnS2.Location = new System.Drawing.Point(697, 362);
             this.btnS2.Name = "btnS2";
             this.btnS2.Size = new System.Drawing.Size(60, 27);
-            this.btnS2.TabIndex = 42;
+            this.btnS2.TabIndex = 32;
             this.btnS2.Text = "S2";
             this.btnS2.UseVisualStyleBackColor = true;
             this.btnS2.Click += new System.EventHandler(this.btnRotate_Click);
@@ -773,7 +773,7 @@
             this.Controls.Add(this.btnU);
             this.Controls.Add(this.btnU2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RubY";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
